b08e227b98fe073ec0485451b9dda2a510b5d17c
volkux <37384192+volkux@users.noreply.github.com>
2023-03-09 23:05:45 -0500

    Maybe?

--- gridsome-config.js
+++ gridsome-config.js
@@ -1,13 +1,26 @@
-
 // This is where project configuration and plugin options are located.
 // Learn more: https://gridsome.org/docs/config
 
 // Changes here require a server restart.
 // To restart press CTRL + C in terminal and run gridsome develop
 
-siteUrl: 'www.zservices.tech'
-
 module.exports = {
   siteName: "ZServices",
-  plugins: []
+  siteUrl: 'https://www.zservices.tech',
+  plugins: [
+    {
+      // Use the dangerous-html package as a plugin
+      use: 'dangerous-html',
+      options: {
+        // Set any options for the plugin here
+      }
+    },
+    {
+      // Use the gridsome package as a plugin
+      use: 'gridsome',
+      options: {
+        // Set any options for the plugin here
+      }
+    }
+  ]
 };
